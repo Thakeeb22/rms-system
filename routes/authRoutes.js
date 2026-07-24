131119d9dfd51530db71bd89add81be4ad511fb9
Muhammad Thakeeb Muhammad <thakeebmuhammad@gmail.com>
2026-07-24 15:34:14 +0100

    delete useless files

--- routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {login} = require("../controllers/authController")
-// login route
-router.post("/login", login)
-module.exports = router
